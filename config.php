64935d6aca68061d277a8b17aec3fe09bd857922
protez76 <60489757+protez76@users.noreply.github.com>
2020-02-01 00:51:53 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'den1.mysql4.gear.host',
+    'port' => 3306,
+    'database' => 'ataberk',
+    'username' => 'ataberk',
+    'password' => 'Jo5yEf-Vg75~',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://soruforum.gearhostpreview.com',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
